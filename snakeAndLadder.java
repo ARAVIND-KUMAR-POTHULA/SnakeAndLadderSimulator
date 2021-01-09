582a22d47740a64b972fb20ea3c0ef49ae6a6168
ARAVIND-KUMAR-POTHULA <pothulaaravind241998@gmail.com>
2021-01-09 23:53:51 +0530

    ADDED. rolling die functionality

--- snakeAndLadder.java
+++ snakeAndLadder.java
@@ -1,7 +1,10 @@
 public class snakeAndLadder {
         public static void main(String[] args) {
                         //variables
-                        int player_Position=0;
-                        System.out.println("PLAYER POSITION = " + player_Position);
+                        int player_Position = 0;
+                        //computation of rolling die
+                        double rolling_Die = Math.floor(Math.random() * 10) % 6;
+                        rolling_Die += 1;
+                        System.out.println("OUTCOME OF ROLLED DIE = " + rolling_Die);
         }
 }
