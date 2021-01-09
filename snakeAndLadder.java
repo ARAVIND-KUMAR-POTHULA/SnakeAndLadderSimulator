47fee82ff33740647e0e128a94234a84ccfedaaf
ARAVIND-KUMAR-POTHULA <pothulaaravind241998@gmail.com>
2021-01-09 23:57:51 +0530

    ADDED. rolling die changing position based on option check functionality

--- snakeAndLadder.java
+++ snakeAndLadder.java
@@ -1,10 +1,27 @@
 public class snakeAndLadder {
-        public static void main(String[] args) {
+
+                public static void main(String[] args) {
+                        //constants
+                         int NO_PLAY = 0;
+                         int IS_LADDER = 1;
+                         int IS_SNAKE = 2;
                         //variables
-                        int player_Position = 0;
+                        int player_Position = 10;
                         //computation of rolling die
-                        double rolling_Die = Math.floor(Math.random() * 10) % 6;
+                        int rolling_Die = (int) Math.floor(Math.random() * 10) % 6;
                         rolling_Die += 1;
-                        System.out.println("OUTCOME OF ROLLED DIE = " + rolling_Die);
+                        //computation for option check
+                        int option_Check = (int) Math.floor(Math.random() * 10) % 3;
+
+                                if (option_Check == IS_LADDER)
+                                        player_Position += rolling_Die;
+                                else if (option_Check == IS_SNAKE)
+                                        player_Position -= rolling_Die;
+                                else if (option_Check == NO_PLAY)
+                                        player_Position = player_Position;
+
+                         System.out.println("option for player check = " + option_Check);
+                         System.out.println("OUTCOME OF ROLLED DIE = " + rolling_Die);
+                         System.out.println("player position after option check = " + player_Position);
         }
 }
