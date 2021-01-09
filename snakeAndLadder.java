bdd0fab43616d89eb45f6faefef84eece61a1e25
ARAVIND-KUMAR-POTHULA <pothulaaravind241998@gmail.com>
2021-01-10 00:02:23 +0530

    ADDED. functionality travel till player position >100

--- snakeAndLadder.java
+++ snakeAndLadder.java
@@ -6,22 +6,24 @@ public class snakeAndLadder {
                          int IS_LADDER = 1;
                          int IS_SNAKE = 2;
                         //variables
-                        int player_Position = 10;
-                        //computation of rolling die
-                        int rolling_Die = (int) Math.floor(Math.random() * 10) % 6;
-                        rolling_Die += 1;
-                        //computation for option check
-                        int option_Check = (int) Math.floor(Math.random() * 10) % 3;
+                        int player_Position = 0;
+                                while (player_Position < 100) {
+                                        //computation of rolling die
+                                        int rolling_Die = (int) Math.floor(Math.random() * 10) % 6;
+                                         rolling_Die += 1;
 
-                                if (option_Check == IS_LADDER)
-                                        player_Position += rolling_Die;
-                                else if (option_Check == IS_SNAKE)
-                                        player_Position -= rolling_Die;
-                                else if (option_Check == NO_PLAY)
-                                        player_Position = player_Position;
+                                         //computation for option check
+                                         int option_Check = (int) Math.floor(Math.random() * 10) % 3;
+                                        if (option_Check == IS_LADDER)
+                                                player_Position += rolling_Die;
+                                        else if (option_Check == IS_SNAKE)
+                                              player_Position -= rolling_Die;
+                                                                 if (player_Position < 0)
+                                                                  player_Position = 0;
+                                        else if (option_Check == NO_PLAY)
+                                                player_Position = player_Position;
 
-                         System.out.println("option for player check = " + option_Check);
-                         System.out.println("OUTCOME OF ROLLED DIE = " + rolling_Die);
-                         System.out.println("player position after option check = " + player_Position);
+                         System.out.println("player position  = " + player_Position);
+                        }
         }
 }
